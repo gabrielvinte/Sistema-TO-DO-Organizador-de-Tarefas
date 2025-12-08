9e78ecddea38a1760613f4863ce5c74db4d6e50a
Layannerib <layanne.ribeiro.5201@gmail.com>
2025-12-08 10:05:28 -0300

    remover-tarefas

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# Remover uma tarefa
+tarefas= ['estudar']
+num = int(input('Digite o número da tarefa que você deseja remover: '))
 
+if 0 <= num < len(tarefas):
+    t_removida = tarefas.pop(num)
+    print(f'Tarefa removida: {t_removida}')
+else:
+    print('Número inválido.')
+
+# Mostra a lista das tarefas que restaram
+print('Lista após remoção da tarefa:')
+for x, tarefa in enumerate(tarefas):
+    print(f'{x} - {tarefa}')
